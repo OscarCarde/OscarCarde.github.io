44118288534ce77f70a0d2fcb3bd6eb730111550
OscarCarde <oscrdns@gmail.com>
2023-07-17 17:14:35 +0100

    added modal resume

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    head = document.createElement('h1');
-    head.innerHTML = "Hello";
-    document.querySelector('body').prepend(head);
+    document.querySelector("#cv").addEventListener('click', ()=>openModal());
+    document.querySelector(".close").addEventListener('click', () =>closeModal());
+    window.addEventListener('click', event => {
+        const modal = document.getElementById('pdfModal');
+        if (event.target === modal) {
+          closeModal();
+        }
+      });
 })
+
+function openModal() {
+    const resume_url = "oscarsresume.pdf";
+    const modal = document.getElementById('pdfModal');
+    const pdfIframe = document.getElementById('pdfIframe');
+    pdfIframe.src = resume_url;
+    modal.style.display = 'block';
+  }
+
+function closeModal() {
+    const modal = document.getElementById('pdfModal');
+    modal.style.display = 'none';
+    // Optionally, you can also clear the iframe content to stop the PDF from loading
+    const pdfIframe = document.getElementById('pdfIframe');
+    pdfIframe.src = '';
+  }
